322d835a516e1e8fe993e557c1b55b2ecec168d6
shmulim <shmuel.markel@gmail.com>
2015-06-08 15:56:47 -0700

    basic routing

---
+++ index.js
@@ -0,0 +1,13 @@
+var spawn = require('child_process').spawn;
+var express = require('express');
+var app = express();
+
+var server = app.listen(3000);
+
+app.get('/:id', function(req, res){
+	var whois = spawn('whois', [req.params.id]);
+	whois.stdout.setEncoding('utf8');
+	whois.stdout.on('data', function(data){
+			res.send(data);
+	});
+});
