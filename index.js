f0f36aaa3ece79706fa8189a14b555e98d10e49a
@shmulim <shmuel.markel@gmail.com>
2015-06-08 17:23:06 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var app = express();
 var server = app.listen(3000);
 
 app.get('/:id', function(req, res){
+	res.setHeader("Content-Type", "application/json");
 	var whois = spawn('whois', [req.params.id]);
 	whois.stdout.setEncoding('utf8');
 	whois.stdout.on('data', function(data){
